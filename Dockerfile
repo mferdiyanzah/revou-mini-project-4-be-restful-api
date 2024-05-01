e2fe4a00aba02f1bacf18e5985889cf969f66ab8
Mohammad Ferdiyanzah <mohammad.ferdiyanzah@dexagroup.com>
2024-05-01 21:05:14 +0700

    update docker

--- Dockerfile
+++ Dockerfile
@@ -3,15 +3,18 @@ FROM node:14.17.0-alpine3.13
 # Create app directory
 WORKDIR /usr
 
-# Copy package.json, tsconfig.json and source code
+# Copy files
 COPY package*.json ./
 COPY tsconfig.json ./
 COPY src ./src
+COPY .env ./
 
+# Install app dependencies
 RUN ls -la
 RUN npm install
 RUN npm run build
 
+# Bundle app source
 RUN npm install -g pm2
 EXPOSE 3000
 CMD ["pm2-runtime", "dist/index.js"]
--- init.sql
+++ init.sql
@@ -20,7 +20,7 @@ CREATE TABLE `movies` (
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `deleted_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE = InnoDB AUTO_INCREMENT = 47 DEFAULT CHARSET = utf8mb3;
+) ENGINE = InnoDB AUTO_INCREMENT = 50 DEFAULT CHARSET = utf8mb3;
 -- cineprime.seats definition
 CREATE TABLE `seats` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -40,7 +40,7 @@ CREATE TABLE `users` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE = InnoDB AUTO_INCREMENT = 9 DEFAULT CHARSET = utf8mb3;
+) ENGINE = InnoDB AUTO_INCREMENT = 12 DEFAULT CHARSET = utf8mb3;
 -- cineprime.movie_casts definition
 CREATE TABLE `movie_casts` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -53,7 +53,7 @@ CREATE TABLE `movie_casts` (
   KEY `actor_id` (`actor_id`),
   CONSTRAINT `movie_casts_ibfk_1` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`id`),
   CONSTRAINT `movie_casts_ibfk_2` FOREIGN KEY (`actor_id`) REFERENCES `actors` (`id`)
-) ENGINE = InnoDB AUTO_INCREMENT = 63 DEFAULT CHARSET = utf8mb3;
+) ENGINE = InnoDB AUTO_INCREMENT = 68 DEFAULT CHARSET = utf8mb3;
 -- cineprime.movie_shows definition
 CREATE TABLE `movie_shows` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -67,7 +67,7 @@ CREATE TABLE `movie_shows` (
   PRIMARY KEY (`id`),
   KEY `movie_id` (`movie_id`),
   CONSTRAINT `movie_shows_ibfk_1` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`id`)
-) ENGINE = InnoDB AUTO_INCREMENT = 22 DEFAULT CHARSET = utf8mb3;
+) ENGINE = InnoDB AUTO_INCREMENT = 32 DEFAULT CHARSET = utf8mb3;
 -- cineprime.showtime_seats definition
 CREATE TABLE `showtime_seats` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -81,14 +81,14 @@ CREATE TABLE `showtime_seats` (
   KEY `seat_id` (`seat_id`),
   CONSTRAINT `showtime_seats_ibfk_1` FOREIGN KEY (`movie_show_id`) REFERENCES `movie_shows` (`id`),
   CONSTRAINT `showtime_seats_ibfk_2` FOREIGN KEY (`seat_id`) REFERENCES `seats` (`id`)
-) ENGINE = InnoDB AUTO_INCREMENT = 34 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+) ENGINE = InnoDB AUTO_INCREMENT = 79 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 -- cineprime.bookings definition
 CREATE TABLE `bookings` (
   `id` int NOT NULL AUTO_INCREMENT,
   `booking_code` varchar(50) NOT NULL,
   `user_id` int NOT NULL,
   `showtime_seat_id` int NOT NULL,
-  `status` enum('pending', 'confirmed', 'cancelled', 'expired') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `status` enum('pending', 'confirmed', 'cancelled', 'expired') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
@@ -97,69 +97,339 @@ CREATE TABLE `bookings` (
   KEY `showtime_seat_id` (`showtime_seat_id`),
   CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `bookings_ibfk_2` FOREIGN KEY (`showtime_seat_id`) REFERENCES `showtime_seats` (`id`)
-) ENGINE = InnoDB AUTO_INCREMENT = 12 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
--- Inserting data into actors
-INSERT INTO `actors` (`name`)
-VALUES ('Actor 1'),
-  ('Actor 2'),
-  ('Actor 3');
--- Inserting data into movies
+) ENGINE = InnoDB AUTO_INCREMENT = 15 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+-- DML
+-- cineprime.actors data
+INSERT INTO `actors` (`id`, `name`, `created_at`, `updated_at`)
+VALUES (
+    1,
+    'Tom Hanks',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00'
+  ),
+  (
+    2,
+    'Tom Cruise',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00'
+  );
+-- cineprime.movies data
 INSERT INTO `movies` (
+    `id`,
     `title`,
     `overview`,
     `duration`,
     `director`,
     `genre`,
     `rating`,
-    `release_date`
+    `release_date`,
+    `created_at`,
+    `updated_at`,
+    `deleted_at`
   )
 VALUES (
-    'Movie 1',
-    'Overview 1',
-    120,
-    'Director 1',
-    'Genre 1',
+    1,
+    'Forrest Gump',
+    'Forrest Gump is a person who despite his low IQ, leads a truly extraordinary life, being present at some of the most important events of the 20th century.',
+    142,
+    'Robert Zemeckis',
+    'Drama',
     'PG-13',
-    '2022-01-01'
+    '1994-07-06',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00',
+    NULL
   ),
   (
-    'Movie 2',
-    'Overview 2',
-    150,
-    'Director 2',
-    'Genre 2',
-    'R',
-    '2022-02-01'
+    2,
+    'Top Gun',
+    'The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills.',
+    110,
+    'Tony Scott',
+    'Action',
+    'PG',
+    '1986-05-16',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00',
+    NULL
   );
--- Inserting data into seats
-INSERT INTO `seats` (`seat_number`)
-VALUES ('A1'),
-  ('A2'),
-  ('A3');
--- Inserting data into users
-INSERT INTO `users` (`username`, `password`, `email`, `is_admin`)
-VALUES ('user1', 'password1', 'user1@example.com', 0),
-  ('admin', 'password2', 'admin@example.com', 1);
--- Inserting data into movie_casts
-INSERT INTO `movie_casts` (`movie_id`, `actor_id`)
-VALUES (1, 1),
-  (1, 2),
-  (2, 3);
--- Inserting data into movie_shows
-INSERT INTO `movie_shows` (`movie_id`, `price`, `show_time`, `status`)
-VALUES (1, 10, '2022-03-01 10:00:00', 'upcoming'),
-  (2, 15, '2022-03-02 12:00:00', 'now_showing');
--- Inserting data into showtime_seats
-INSERT INTO `showtime_seats` (`movie_show_id`, `seat_id`, `status`)
-VALUES (1, 1, 'available'),
-  (1, 2, 'booked'),
-  (2, 3, 'available');
--- Inserting data into bookings
-INSERT INTO `bookings` (
-    `booking_code`,
-    `user_id`,
-    `showtime_seat_id`,
+-- cineprime.movie_casts data
+INSERT INTO `movie_casts` (
+    `id`,
+    `movie_id`,
+    `actor_id`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES (
+    1,
+    1,
+    1,
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00'
+  ),
+  (
+    2,
+    2,
+    2,
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00'
+  );
+-- cineprime.movie_shows data
+INSERT INTO `movie_shows` (
+    `id`,
+    `movie_id`,
+    `price`,
+    `show_time`,
+    `created_at`,
+    `updated_at`,
+    `deleted_at`,
     `status`
   )
-VALUES ('BOOK123', 1, 1, 'pending'),
-  ('BOOK456', 2, 2, 'confirmed');
+VALUES (
+    1,
+    1,
+    100000,
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00',
+    NULL,
+    'now_showing'
+  ),
+  (
+    2,
+    2,
+    100000,
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00',
+    NULL,
+    'now_showing'
+  );
+-- cineprime.seats data
+INSERT INTO `seats` (
+    `id`,
+    `seat_number`,
+    `created_at`,
+    `updated_at`,
+    `deleted_at`
+  )
+VALUES (
+    1,
+    'A1',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00',
+    NULL
+  ),
+  (
+    2,
+    'A2',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00',
+    NULL
+  ),
+  (
+    3,
+    'A3',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00',
+    NULL
+  ),
+  (
+    4,
+    'A4',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00',
+    NULL
+  ),
+  (
+    5,
+    'A5',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00',
+    NULL
+  ),
+  (
+    6,
+    'A6',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00',
+    NULL
+  ),
+  (
+    7,
+    'A7',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00',
+    NULL
+  ),
+  (
+    8,
+    'A8',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00',
+    NULL
+  ),
+  (
+    9,
+    'A9',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00',
+    NULL
+  ),
+  (
+    10,
+    'A10',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00',
+    NULL
+  ),
+  (
+    11,
+    'A11',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00',
+    NULL
+  ),
+  (
+    12,
+    'A12',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00',
+    NULL
+  ),
+  (
+    13,
+    'A13',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00',
+    NULL
+  ),
+  (
+    14,
+    'A14',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00',
+    NULL
+  );
+-- cineprime.showtime_seats data
+INSERT INTO `showtime_seats` (
+    `id`,
+    `movie_show_id`,
+    `seat_id`,
+    `status`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES (
+    1,
+    1,
+    1,
+    'available',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00'
+  ),
+  (
+    2,
+    1,
+    2,
+    'available',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00'
+  ),
+  (
+    3,
+    1,
+    3,
+    'available',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00'
+  ),
+  (
+    4,
+    1,
+    4,
+    'available',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00'
+  ),
+  (
+    5,
+    1,
+    5,
+    'available',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00'
+  ),
+  (
+    6,
+    1,
+    6,
+    'available',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00'
+  ),
+  (
+    7,
+    1,
+    7,
+    'available',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00'
+  ),
+  (
+    8,
+    1,
+    8,
+    'available',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00'
+  ),
+  (
+    9,
+    1,
+    9,
+    'available',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00'
+  ),
+  (
+    10,
+    1,
+    10,
+    'available',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00'
+  ),
+  (
+    11,
+    1,
+    11,
+    'available',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00'
+  ),
+  (
+    12,
+    1,
+    12,
+    'available',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00'
+  ),
+  (
+    13,
+    1,
+    13,
+    'available',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00'
+  ),
+  (
+    14,
+    1,
+    14,
+    'available',
+    '2021-09-01 00:00:00',
+    '2021-09-01 00:00:00'
+  );
